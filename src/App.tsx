227e621e0def8a30f171b692e590710fffaf4d9c
Diego-monfort-landot <diegomonfort1989@gmail.com>
2023-12-12 19:59:24 +0100

    added types.d and interface state, todoo is the action type

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,67 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
+import { useReducer } from 'react'
+import { type State } from './types'
+
+// create a initial state
+
+const initialState: State = {
+  fromLanguage: 'auto',
+  toLanguage: 'en',
+  fromText: '',
+  loading: false
+}
+
+// create a reducer
+
+function reducer (state: State, action) {
+  const { type, payload } = action
+
+  if (type === 'INTERCHANGE_LANGUAGE') {
+    return {
+      ...state,
+      fromLanguage: state.toLanguage,
+      toLanguagee: state.fromLanguage
+    }
+  }
+
+  if (type === 'SET_FROM_LANGUAGE') {
+    return {
+      ...state,
+      fromLanguage: payload
+    }
+  }
+
+  if (type === 'SET_TO_LANGUAGE') {
+    return {
+      ...state,
+      toLanguage: payload
+    }
+  }
+
+  if (type === 'SET_FROM_TEXT') {
+    return {
+      ...state,
+      loading: true,
+      result: payload,
+      result: ''
+    }
+  }
+
+  if (type === 'SET_RESULT') {
+    return {
+      ...state,
+      loading: false,
+      result: payload
+    }
+  }
+  return state
+}
 
 function App () {
+  // use the state useReducer
+
+  const [state, dispatch] = useReducer(reducer, initialState)
   return (
     <>
       <h1>Google Translate</h1>
---
+++ src/types.d.ts
@@ -0,0 +1,6 @@
+export interface State {
+  fromLanguage: string
+  toLanguage: string
+  fromText: string
+  loading: boolean
+}
